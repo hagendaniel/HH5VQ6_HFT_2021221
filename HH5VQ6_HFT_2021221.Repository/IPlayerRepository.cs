122a2d1cc71b12ba17b93363e1e358e57c02fbd9
hagendaniel <hagen.daniel12@gmail.com>
2021-11-07 11:09:13 +0100

    Separated interfaces class into custom interface classes

---
+++ HH5VQ6_HFT_2021221.Repository/IPlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using HH5VQ6_HFT_2021221.Models;
+
+namespace HH5VQ6_HFT_2021221.Repository
+{
+    public interface IPlayerRepository : IRepository<Player>
+    {
+        void registerNewPlayer(Player newPlayer);
+        void removePlayer(int id);
+        void changeStatus(int id, bool newStatus, string eliminatedOnMap);
+        void customId(int id, int customId);
+    }
+}
