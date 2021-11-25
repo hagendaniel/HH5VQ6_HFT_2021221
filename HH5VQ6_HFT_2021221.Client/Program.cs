24fec580b9b8038d52155d2d7be53ade30937f88
hagendaniel <hagen.daniel12@gmail.com>
2021-11-25 23:16:04 +0100

    ConsoleMenu of Maps has been added (CRUD)

--- HH5VQ6_HFT_2021221.Client/Program.cs
+++ HH5VQ6_HFT_2021221.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using HH5VQ6_HFT_2021221.Models;
+﻿using ConsoleTools;
+using HH5VQ6_HFT_2021221.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -90,6 +91,15 @@ namespace HH5VQ6_HFT_2021221.Client
 
             //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
+            var mapMenu = new ConsoleMenu()
+                .Add("Create new map", () => CreateMap(rest))
+                .Add("Show all maps", () => GetAllMaps(rest))
+                .Add("Show a map by id", () => GetMap(rest))
+                .Add("Rename a map", () => RenameMap(rest))
+                .Add("Delete a map", () => DeleteMap(rest));
+
+            mapMenu.Show();
+
             Console.ReadKey();
         }
 
@@ -123,7 +133,66 @@ namespace HH5VQ6_HFT_2021221.Client
         //    }
 
         //    throw new Exception();
-            
+
         //}
+
+        //CRUD METHODS FOR THE MENU-----------------------------------------------------------------------------------------
+        //CRUD METHODS FOR THE MAPMENU--------------------------------------------------------------------------------------
+        private static void CreateMap(RestService restService)
+        {
+            Console.WriteLine("Name of the new map: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Difficulty of the new map (1-10): ");
+            int difficulty = int.Parse(Console.ReadLine());
+            restService.Post(new Map
+            {
+                MapName = name,
+                Difficulty=difficulty
+            }, "maps");
+            Console.WriteLine("The new map has been created");
+            Console.ReadKey();
+        }
+
+        private static void GetAllMaps(RestService restService)
+        {
+            var maps = restService.Get<Map>("maps");
+            foreach (var map in maps)
+            {
+                Console.WriteLine(map.MapName);
+            }
+            Console.ReadKey();
+        }
+
+        private static void GetMap(RestService restService)
+        {
+            Console.WriteLine("Id of the map: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Map map = restService.Get<Map>(id, "maps");
+            Console.WriteLine($"The map with the given id is {map.MapName}, with difficulty level of {map.Difficulty}");
+            Console.ReadKey();
+        }
+
+        private static void RenameMap(RestService restService)
+        {
+            Console.WriteLine("Id of the map you want to rename: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Map map = restService.Get<Map>(id, "maps");
+            string oldName = map.MapName;
+            Console.WriteLine("New name of the map: ");
+            string newName = Console.ReadLine();
+            map.MapName = newName;
+            restService.Put(map,"maps");
+            Console.WriteLine($"The map has been renamed from {oldName} to {newName}");
+            Console.ReadKey();
+        }
+
+        private static void DeleteMap(RestService restService)
+        {
+            Console.WriteLine("Id of the map you want to delete: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            restService.Delete(id,"maps");
+            Console.WriteLine($"The map with the id of ({id}) has been deleted.");
+            Console.ReadKey();
+        }
     }
 }
