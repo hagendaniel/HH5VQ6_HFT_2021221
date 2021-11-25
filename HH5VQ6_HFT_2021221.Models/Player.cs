30568d0ec0c47b74ac115b256527f7f527fac5be
hagendaniel <hagen.daniel12@gmail.com>
2021-11-25 23:18:25 +0100

    Changed from Newtonsoft to System.Text.Json because of [JsonIgnore]

--- HH5VQ6_HFT_2021221.Models/Player.cs
+++ HH5VQ6_HFT_2021221.Models/Player.cs
@@ -1,10 +1,10 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HH5VQ6_HFT_2021221.Models
